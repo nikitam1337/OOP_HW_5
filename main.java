9ced9dbb16a534cef748f360752c26af1ffc0336
nikitam1337 <nikitam.l@mail.ru>
2023-03-25 22:44:22 +0300

    HW 5 compleated

--- main.java
+++ main.java
@@ -4,33 +4,13 @@ package OOP_HW_5;
 import java.lang.reflect.InvocationHandler;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Spliterator;
+
+import OOP_HW_5.RobotMap.Direction;
 
 public class Main {
 
     public static void main(String[] args) {
-        // 1.
-        // Карта с каким-то размером nxm.
-        // На ней можно создать робов, указывая начальное положение.
-        // Если начальное положение некорректное ИЛИ эта позиция занята другим робом, то
-        // кидаем исключение.
-        // Робот имеет направление (вверх, вправо, вниз, влево). У роботов можно менять
-        // направление и передвигать их на 1 шаг вперед
-        // 2.
-        // Написать контроллер к этому коду, который будет выступать посредником между
-        // консолью (пользователем) и этой игрой.
-        // (0,0) ------------------ (0, m)
-        // ...
-        // (n, 0) ----------------------- (n, m)
-
-        // Robot, Map, Point
-
-        // Домашнее задание:
-        // Реализовать чтение команд с консоли и выполнить их в main методе
-        // Список команд:
-        // create-map 3 5 -- РЕАЛИЗОВАНО!
-        // create-robot 2 7
-        // move-robot id
-        // change-direction id LEFT
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите команду 'create-map m n'(где m и n -размеры карты) для создания карты:");
@@ -54,15 +34,19 @@ public class Main {
         }
         System.out.println("Карта создана успешно!");
         System.out.println("ИГРАЕМ...");
+        System.out.println(Direction.LEFT);
 
-        // Scanner sc2 = new Scanner(System.in);
-        System.out.println(
-                "Список команд:\n" +
-                        "1. create-robot m n (где m и n - расположение робота) для создания робота.\n" +
-                        "2. move-robot id - для перемещения робота на 1 шаг.\n" +
-                        "3. change-direction id LEFT - изменить направление движения нужного робота\n");
-        RobotMap.Robot robot = null;
         while (true) {
+            System.out.println();
+            System.out.println(
+                    "Список команд:\n" +
+                            "1. create-robot m n (где m и n - расположение робота) - для создания робота.\n" +
+                            "2. move-robot id - для перемещения робота на 1 шаг.\n" +
+                            "3. change-direction id LEFT (TOP, RIGHT, BOTTOM, LEFT) - изменить направление движения нужного робота\n"
+                            +
+                            "4. show-all-robots - для показа списка существующих роботов и их позиций на карте.\n" +
+                            "5. q - завершение программы!\n");
+            RobotMap.Robot robot = null;
             String command = sc.nextLine();
             if (command.startsWith("create-robot")) {
                 String[] split = command.split(" ");
@@ -71,8 +55,6 @@ public class Main {
                 try {
                     robot = map.createRobot(new Point(Integer.parseInt(arguments[0]), Integer.parseInt(arguments[1])));
                     System.out.println("Робот №" + robot.getId() + " успешно создан и помещен на карту.");
-                    //System.out.println(robot); - выводит в консоль робота в формате: "[id] [m,n]"
-                    break;
                 } catch (PositionException e) {
                     System.out.println("Во время создания робота случилось исключение: " + e.getMessage() + "." +
                             " Попробуйте еще раз");
@@ -80,23 +62,55 @@ public class Main {
             } else if (command.startsWith("move-robot")) {
                 String[] split = command.split(" ");
                 long robotId = Long.parseLong(split[1]);
+                robot = map.getRobotById(robotId);
 
-                // Если robots в RobotMap содержит id равный полученному, то двигаем нашего
-                // робота. Иначе пишем об отсутствии робота с таким id.
+                if (robot == null) {
+                    System.out.println("Робота с идентификатором " + robotId + "Не найден. Задайте другой id");
+                } else {
+                    try {
+                        robot.move();
+                        System.out.println("Робот №" + robot.getId() + " успешно двигается на шаг вперед.");
+                        // System.out.println(robot);
+                    } catch (PositionException e) {
+                        System.out.println("Во время движения робота случилось исключение: " + e.getMessage() + "." +
+                                " Попробуйте еще раз");
+                    }
+                }
 
             } else if (command.startsWith("change-direction")) {
                 String[] split = command.split(" ");
+                String[] argumenst = Arrays.copyOfRange(split, 1, split.length);
                 long robotId = Long.parseLong(split[1]);
-                
-                // Если robots в RobotMap содержит id равный полученному, то меняем роботу направление.
-                //Иначе пишем, что такого робота нет
+                String direction = argumenst[1];
+
+                robot = map.getRobotById(robotId);
+
+                if (robot == null) {
+                    System.out.println("Робота с идентификатором " + robotId + "Не найден. Задайте другой id");
+                } else {
+                    try {
+                        robot.changeDirection(direction);
+                        System.out.println("Робот №" + robot.getId() + " сменил направление на " + direction);
+                        System.out.println(robot);
+                    } catch (DirectionException e) {
+                        System.out.println("Во время смены направления случилось исключение: " + e.getMessage() + "." +
+                                " Попробуйте еще раз");
+                    }
+                }
+
+            } else if (command.equals("show-all-robots")) {
+                System.out.println("Список всех ротботов на карте:");
+                System.out.println();
+                map.printAllRobots();
+
+            } else if (command.equals("q")) {
+                System.out.println("Завершение программы....");
+                break;
 
             } else {
                 System.out.println("Команда не найдена. Попробуйте еще раз");
             }
         }
-
-        // map.getAllRobots();  - выводит список всех роботов в формате: "ID: ххх, Position: [m,n]"
 
         sc.close();
     }
